9795389a9f2e91b9258127d304d4e46432c38e95
Warren Harding <whrdng@gmail.com>
2022-04-22 21:15:22 -0500

    infra: add method to grant permissions
    Create function on datastore-construct
    to grant permissions to Lambda fns

--- lib/constructs/shared-datastore-construct.ts
+++ lib/constructs/shared-datastore-construct.ts
@@ -1,5 +1,6 @@
 import { CfnOutput, RemovalPolicy } from "aws-cdk-lib";
 import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 
 export class SharedDatastore extends Construct {
@@ -8,7 +9,7 @@ export class SharedDatastore extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    this.ddbTable = new Table(this, "app-table", {
+    this.ddbTable = new Table(this, "table", {
       partitionKey: { name: "PK", type: AttributeType.STRING },
       sortKey: { name: "SK", type: AttributeType.STRING },
       pointInTimeRecovery: true,
@@ -16,8 +17,13 @@ export class SharedDatastore extends Construct {
       removalPolicy: RemovalPolicy.DESTROY,
     });
 
-    new CfnOutput(this, "DynamoDB table name", {
+    new CfnOutput(this, "Dynamodb-Table-Name", {
+      exportName: "DynamoDB-Table-Name",
       value: this.ddbTable.tableName,
     });
+  }
+
+  grantReadWritePermissions(functions: NodejsFunction[]) {
+    functions.forEach((fn) => this.ddbTable.grantReadWriteData(fn));
   }
 }
