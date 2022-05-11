73bd2cd4415b3b365e3bd1ccf35d17ad7415c73a
Warren Harding <whrdng@gmail.com>
2022-05-11 03:14:46 -0500

    infra: update lambda to pull data correctly

--- src/Budgeting/infrastructure/lambda/add-trxn-fn.ts
+++ src/Budgeting/infrastructure/lambda/add-trxn-fn.ts
@@ -7,27 +7,26 @@ import { TransactionSnapshot } from "../../model/types";
 import initDocClient from "../repo/init-doc-client";
 import { getEnvVar } from "./utils/get-env-var";
 
-type AddTransactionHandler = Handler<
-  any,
-  TransactionSnapshot | string
->;
+type AddTransactionHandler = Handler<any, TransactionSnapshot | string>;
 
 let docClient: DynamoDBDocumentClient;
 docClient ??= initDocClient();
 let tableName = getEnvVar("DATABASE_NAME");
 
-export const handler: AddTransactionHandler = async (event) => {
+export const handler: AddTransactionHandler = async (event, context) => {
   const repo = new DDBTransactionRepo({
     client: docClient,
     tableName: tableName,
   });
+
+  const input = event.arguments.input;
   const addTransactionDTO: AddTransactionDTO = {
-    userId: event.userId,
-    date: event.date,
-    currencyCode: event.currencyCode,
-    amount: event.amount,
-    memo: event.memo,
-    vendor: event.vendor,
+    userId: input.userId,
+    date: input.date,
+    currencyCode: input.currencyCode,
+    amount: input.amount,
+    memo: input.memo,
+    vendor: input.vendor,
   };
   const result = await addTransaction(addTransactionDTO, repo);
   return result.isErr() ? result.error.message : result.value;
